eeda4edfc57476e16489e9712430241fbc9c98a5
ltoker <lilchpilch@yahoo.com>
2021-07-05 15:04:00 +0200

    Check if works

---
+++ SetGit.R
@@ -0,0 +1,25 @@
+
+## set your user name and email:
+usethis::use_git_config(user.name = "ltoker", user.email = "lilchpilch@yahoo.com")
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## set personal access token:
+credentials::set_github_pat("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")
+
+## or store it manually in '.Renviron':
+usethis::edit_r_environ()
+## store your personal access token with: GITHUB_PAT=xxxyyyzzz
+## and make sure '.Renviron' ends with a newline
+
+# ----------------------------------------------------------------------------
+
+#### 4. Verify settings ######################################################
+
+usethis::git_sitrep()
+
+## Your username and email should be stated correctly in the output. 
+## Also, the report shoud cotain something like:
+## 'Personal access token: '<found in env var>''
